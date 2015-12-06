271e1f6d848b541b94df433f476afd638cd5a3a8
stolen data <djinn@stolendata.net>
2015-12-07 00:20:38 +0100

    0.8.0: database storage

--- README.md
+++ README.md
@@ -1,40 +1,49 @@
 RPC Ace (RPC AnyCoin Explorer)
 ==============================
 
-Copyright (c) 2014 Robin Leffmann
+Copyright (c) 2014 - 2015 Robin Leffmann
 
-RPC Ace is a simple alternative block explorer written in PHP. It does not generate a database, and it interacts with block chains entirely via RPC, either against a locally running wallet/daemon or remotely over the Internet.
+RPC Ace is a simple alternative block explorer written in PHP. It interacts with block chains entirely via RPC, either against a locally running wallet/daemon or remotely over the Internet, and offers optional database storage for quickly accessing previously processed blocks and transactions.
 
-A database-less explorer has a few drawbacks - most notably, RPC Ace cannot keep track of addresses or total coins generated, and as it uses RPC calls to parse blocks a transaction-heavy block chain (such as Bitcoin) can incur heavy CPU usage and/or long page generation times. RPC Ace's primary use is quick access to oversight of a block chain; for in-depth needs it's recommended to run a tallying explorer such as Abe.
+The lightweight nature of RPC Ace comes with a few drawbacks - most notably, the explorer cannot keep track of addresses or total coins generated, and as it uses RPC calls to parse blocks a transaction-heavy block chain (such as Bitcoin) can incur heavy CPU usage and/or long page generation times the first time a set of blocks is parsed. RPC Ace's primary use is quick access to oversight of a block chain; for in-depth needs it's recommended to run a tallying explorer such as Abe.
 
 RPC Ace should work with any block chain regardless of what proof-of-work algorithm is used, and has been tested to work with Bitcoin, Litecoin, Dogecoin, Solcoin and a few other block chains, but as it's still at an early stage it may contain bugs. Version 0.6.5 introduced PoS support, which has been tested against a number of popular PoS block chains.
 
-Version 0.7.0 introduced optional JSON output through a rewrite of the codebase which split the project into an example block explorer and a class, `RPCAce`, with two functions, `getBlock` and `getBlockList`, for returning a PHP array or JSON of a block's details and a summarized list of blocks respectively. This change allows for removing the example block explorer of RPC Ace in order to use the RPCAce class on its own for processing and presenting block chain data in other ways.
+Version 0.7.0 introduced optional JSON output through a rewrite of the codebase which split the project into an example block explorer and a class, `RPCAce`, with two functions, `get_block` and `get_blocklist`, for returning a PHP array or JSON of a block's details and a summarized list of blocks respectively. This change allows for removing the example block explorer of RPC Ace in order to use the RPCAce class on its own for processing and presenting block chain data in other ways.
+
+Version 0.8.0 introduced optional database storage via SQLite, offering faster page generation and reduced burden on the node serving RPC requests to the explorer.
 
 Setting up RPC Ace
 ------------------
 
-RPC Ace (and the extras) requires PHP version 5.4 or later, with CURL and JSON support enabled.
+RPC Ace (and the extras) requires PHP version 5.4 or later, with CURL and JSON support enabled. Additionally, SQLite and zlib support is required to use the database storage feature.
 
 Place `rpcace.php` and `easybitcoin.php` ([get it here](https://github.com/aceat64/EasyBitcoin-PHP)) together in your web directory. The first few lines of `rpcace.php` defines its configurable parameters:
 
-    RPCHOST, '127.0.0.1'                  // Host/IP for the daemon
-    RPCPORT, 12345                        // RPC port for the daemon
-    RPCUSER, 'username'                   // 'rpcuser' from the coin's .conf
-    RPCPASS, 'password'                   // 'rpcpassword' from the coin's .conf
+    RPC_HOST = '127.0.0.1'              // Host/IP for the daemon
+    RPC_PORT = 12345                    // RPC port for the daemon
+    RPC_USER = 'username'               // 'rpcuser' from the coin's .conf
+    RPC_PASS = 'password'               // 'rpcpassword' from the coin's .conf
 
-    COINNAME, 'Somecoin'                  // Coin name/title
-    COINPOS, false                        // Set to true for proof-of-stake coins
+    COIN_NAME = 'Somecoin'              // Coin name/title
+    COIN_POS = false                    // Set to true for proof-of-stake coins
 
-    RETURNJSON, false                     // Set to true to return JSON instead of PHP arrays
-    DATEFORMAT, 'H:i:s Y-M-d'             // Date format for block list
-    BLOCKSPERLIST, 12                     // Number of blocks to collect for the block list
+    RETURN_JSON = false                 // Set to true to return JSON instead of PHP arrays
+    DATE_FORMAT = 'Y-M-d H:i:s'         // Date format for blocklist
+    BLOCKS_PER_LIST = 12                // Number of blocks to collect for the blocklist
 
-    COINHOME, 'http://www.somecoin.org/'  // Coin website
-    REFRESHTIME, 180                      // Seconds between automatic HTML page refresh
+    DB_FILE = 'db/somecoin_db.sq3';     // Set to false to disable database storage
 
+    // for the example explorer
+    COIN_HOME = 'http://www.coin.org/'  // Coin website
+    REFRESH_TIME = 180                  // Seconds between automatic HTML page refresh
+
+
+For databaste storage it usually suffices to create a directory that is owned and writable by the user the httpd process runs under, and pointing the `DB_FILE` setting to a suitable filename inside that directory.
 
 To get accurate transaction values your block chain must be reindexed (or built from scratch) with full transaction indexing, by setting `txindex=1` in the coin's .conf file.
+
+Additional help and advice might be found in the official thread on the BitcoinTalk forum: https://bitcointalk.org/index.php?topic=686177.0
 
 
 Extras
--- rpcace.php
+++ rpcace.php
@@ -1,52 +1,64 @@
 <?php
 /*
-    RPC Ace v0.7.1 (RPC AnyCoin Explorer)
+    RPC Ace v0.8.0 (RPC AnyCoin Explorer)
 
-    (c) 2014 Robin Leffmann <djinn at stolendata dot net>
+    (c) 2014 - 2015 Robin Leffmann <djinn at stolendata dot net>
 
     https://github.com/stolendata/rpc-ace/
 
     licensed under CC BY-NC-SA 4.0 - http://creativecommons.org/licenses/by-nc-sa/4.0/
 */
 
-define( 'ACEVERSION', '0.7.1' );
+const ACE_VERSION = '0.8.0';
 
-define( 'RPCHOST', '127.0.0.1' );
-define( 'RPCPORT', 12345 );
-define( 'RPCUSER', 'username' );
-define( 'RPCPASS', 'password' );
+const RPC_HOST = '127.0.0.1';
+const RPC_PORT = 12345;
+const RPC_USER = 'username';
+const RPC_PASS = 'password';
 
-define( 'COINNAME', 'Somecoin' );
-define( 'COINPOS', false );
+const COIN_NAME = 'Somecoin';
+const COIN_POS = false;
 
-define( 'RETURNJSON', false );
-define( 'DATEFORMAT', 'H:i:s Y-M-d' );
-define( 'BLOCKSPERLIST', 12 );
+const RETURN_JSON = false;
+const DATE_FORMAT = 'Y-M-d H:i:s';
+const BLOCKS_PER_LIST = 12;
+
+const DB_FILE = 'db/somecoin_db.sq3';
 
 // for the example explorer
-define( 'COINHOME', 'http://www.somecoin.org/' );
-define( 'REFRESHTIME', 180 );
+const COIN_HOME = 'https://www.coin.org/';
+const REFRESH_TIME = 180;
 
 // courtesy of https://github.com/aceat64/EasyBitcoin-PHP/
 require_once( 'easybitcoin.php' );
 
 class RPCAce
 {
-    private static $blockFields = [ 'hash', 'nextblockhash', 'previousblockhash', 'confirmations', 'size', 'height', 'version', 'merkleroot', 'time', 'nonce', 'bits', 'difficulty', 'mint', 'proofhash' ];
+    private static $block_fields = [ 'hash', 'nextblockhash', 'previousblockhash', 'confirmations', 'size', 'height', 'version', 'merkleroot', 'time', 'nonce', 'bits', 'difficulty', 'mint', 'proofhash' ];
 
     private static function base()
     {
-        $rpc = new Bitcoin( RPCUSER, RPCPASS, RPCHOST, RPCPORT );
+        $rpc = new Bitcoin( RPC_USER, RPC_PASS, RPC_HOST, RPC_PORT );
         $info = $rpc->getinfo();
         if( $rpc->status !== 200 && $rpc->error !== '' )
             return [ 'err'=>'failed to connect - node not reachable, or user/pass incorrect' ];
 
-        $output['rpcace_version'] = ACEVERSION;
-        $output['coin_name'] = COINNAME;
+        if( DB_FILE )
+        {
+            $pdo = new PDO( 'sqlite:' . DB_FILE );
+            $pdo->exec( 'create table if not exists block ( height int, hash char(64), json blob );
+                         create table if not exists tx ( txid char(64), json blob );
+                         create unique index if not exists ub on block ( height );
+                         create unique index if not exists uh on block ( hash );
+                         create unique index if not exists ut on tx ( txid );' );
+        }
+
+        $output['rpcace_version'] = ACE_VERSION;
+        $output['coin_name'] = COIN_NAME;
         $output['num_blocks'] = $info['blocks'];
         $output['num_connections'] = $info['connections'];
 
-        if( COINPOS === true )
+        if( COIN_POS === true )
         {
             $output['current_difficulty_pow'] = $info['difficulty']['proof-of-work'];
             $output['current_difficulty_pos'] = $info['difficulty']['proof-of-stake'];
@@ -58,30 +70,77 @@ class RPCAce
             $hashRate = $rpc->getnetworkhashps() / 1000000;
         $output['hashrate_mhps'] = sprintf( '%.2f', $hashRate );
 
-        return [ 'output'=>$output, 'rpc'=>$rpc ];
+        return [ 'output'=>$output, 'rpc'=>$rpc, 'pdo'=>@$pdo ];
+    }
+
+    private static function block( $base, $b )
+    {
+        if( DB_FILE )
+        {
+            $sth = $base['pdo']->prepare( 'select json from block where height = ? or hash = ?;' );
+            $sth->execute( [$b, $b] );
+            $block = $sth->fetchColumn();
+            if( $block )
+                $block = json_decode( gzinflate($block), true );
+        }
+        if( @$block == false )
+        {
+            if( strlen($b) < 64 )
+                $b = $base['rpc']->getblockhash( $b );
+            $block = $base['rpc']->getblock( $b );
+        }
+
+        if( DB_FILE && @$block )
+        {
+            $sth = $base['pdo']->prepare( 'insert into block values (?, ?, ?);' );
+            $sth->execute( [$block['height'], $block['hash'], gzdeflate(json_encode($block))] );
+        }
+
+        return $block ? $block : false;
+    }
+
+    private static function tx( $base, $txid )
+    {
+        if( DB_FILE )
+        {
+            $sth = $base['pdo']->prepare( 'select json from tx where txid = ?;' );
+            $sth->execute( [$txid] );
+            $tx = $sth->fetchColumn();
+            if( $tx )
+                $tx = json_decode( gzinflate($tx), true );
+        }
+        if( @$tx == false )
+            $tx = $base['rpc']->getrawtransaction( $txid, 1 );
+
+        if( DB_FILE && @$tx )
+        {
+            $sth = $base['pdo']->prepare( 'insert into tx values (?, ?);' );
+            $sth->execute( [$txid, gzdeflate(json_encode($tx))] );
+        }
+
+        return $tx ? $tx : false;
     }
 
     // enumerate block details from hash
-    public static function getBlock( $hash )
+    public static function get_block( $hash )
     {
-        if( preg_match('/^([[:xdigit:]]{64})$/', $hash) !== 1 )
-            return RETURNJSON ? json_encode( ['err'=>'not a valid block hash'] ) : [ 'err'=>'not a valid block hash' ];
+        if( preg_match('/^[0-9a-f]{64}$/i', $hash) !== 1 )
+            return RETURN_JSON ? json_encode( ['err'=>'not a valid block hash'] ) : [ 'err'=>'not a valid block hash' ];
 
         $base = self::base();
         if( isset($base['err']) )
-            return RETURNJSON ? json_encode( $base ) : $base;
+            return RETURN_JSON ? json_encode( $base ) : $base;
 
-        if( ($block = $base['rpc']->getblock($hash)) === false )
-            return RETURNJSON ? json_encode( ['err'=>'no block with that hash'] ) : [ 'err'=>'no block with that hash' ];
+        if( ($block = self::block($base, $hash)) === false )
+            return RETURN_JSON ? json_encode( ['err'=>'no block with that hash'] ) : [ 'err'=>'no block with that hash' ];
 
         $total = 0;
         foreach( $block as $id => $val )
             if( $id === 'tx' )
                 foreach( $val as $txid )
                 {
-                    $transaction = array();
                     $transaction['id'] = $txid;
-                    if( ($tx = $base['rpc']->getrawtransaction($txid, 1)) === false )
+                    if( ($tx = self::tx($base, $txid)) === false )
                         continue;
 
                     if( isset($tx['vin'][0]['coinbase']) )
@@ -95,43 +154,43 @@ class RPCAce
                             $transaction['outputs'][$entry['n']]['address'] = $entry['scriptPubKey']['addresses'][0];
                         }
                     $base['output']['transactions'][] = $transaction;
+                    $transaction = null;
                 }
-            elseif( in_array($id, self::$blockFields) )
+            elseif( in_array($id, self::$block_fields) )
                 $base['output']['fields'][$id] = $val;
 
         $base['output']['total_out'] = $total;
         $base['rpc'] = null;
-        return RETURNJSON ? json_encode( $base['output'] ) : $base['output'];
+        return RETURN_JSON ? json_encode( $base['output'] ) : $base['output'];
     }
 
     // create summarized list from block number
-    public static function getBlockList( $ofs, $n = BLOCKSPERLIST )
+    public static function get_blocklist( $ofs, $n = BLOCKS_PER_LIST )
     {
         $base = self::base();
         if( isset($base['err']) )
-            return RETURNJSON ? json_encode( $base ) : $base;
+            return RETURN_JSON ? json_encode( $base ) : $base;
 
         $offset = $ofs === null ? $base['output']['num_blocks'] : abs( (int)$ofs );
         if( $offset > $base['output']['num_blocks'] )
-            return RETURNJSON ? json_encode( ['err'=>'block does not exist'] ) : [ 'err'=>'block does not exist' ];
+            return RETURN_JSON ? json_encode( ['err'=>'block does not exist'] ) : [ 'err'=>'block does not exist' ];
 
         $i = $offset;
         while( $i >= 0 && $n-- )
         {
-            $frame = array();
-            $frame['hash'] = $base['rpc']->getblockhash( $i );
-            $block = $base['rpc']->getblock( $frame['hash'] );
+            $block = self::block( $base, $i );
+            $frame['hash'] = $block['hash'];
             $frame['height'] = $block['height'];
             $frame['difficulty'] = $block['difficulty'];
             $frame['time'] = $block['time'];
-            $frame['date'] = gmdate( DATEFORMAT, $block['time'] );
+            $frame['date'] = gmdate( DATE_FORMAT, $block['time'] );
 
             $txCount = 0;
             $valueOut = 0;
             foreach( $block['tx'] as $txid )
             {
                 $txCount++;
-                if( ($tx = $base['rpc']->getrawtransaction($txid, 1)) === false )
+                if( ($tx = self::tx($base, $txid)) === false )
                     continue;
                 foreach( $tx['vout'] as $vout )
                     $valueOut += $vout['value'];
@@ -140,11 +199,12 @@ class RPCAce
             $frame['total_out'] = $valueOut;
 
             $base['output']['blocks'][] = $frame;
+            $frame = null;
             $i--;
         }
 
         $base['rpc'] = null;
-        return RETURNJSON ? json_encode( $base['output'] ) : $base['output'];
+        return RETURN_JSON ? json_encode( $base['output'] ) : $base['output'];
     }
 }
 ?>
@@ -158,23 +218,23 @@ class RPCAce
 $query = substr( @$_SERVER['QUERY_STRING'], 0, 64 );
 
 if( strlen($query) == 64 )
-    $ace = RPCAce::getBlock( $query );
+    $ace = RPCAce::get_block( $query );
 else
 {
     $query = ( $query === false || !is_numeric($query) ) ? null : abs( (int)$query );
-    $ace = RPCAce::getBlockList( $query, BLOCKSPERLIST );
+    $ace = RPCAce::get_blocklist( $query, BLOCKS_PER_LIST );
     $query = $query === null ? @$ace['num_blocks'] : $query;
 }
 
-if( isset($ace['err']) || RETURNJSON === true )
-    die( 'RPC Ace error: ' . (RETURNJSON ? $ace : $ace['err']) );
+if( isset($ace['err']) || RETURN_JSON === true )
+    die( 'RPC Ace error: ' . (RETURN_JSON ? $ace : $ace['err']) );
 
 echo <<<END
 <!DOCTYPE html>
 <!--
-    RPC Ace v0.7.1 (RPC AnyCoin Explorer)
+    RPC Ace v0.8.0 (RPC AnyCoin Explorer)
 
-    (c) 2014 Robin Leffmann <djinn at stolendata dot net>
+    (c) 2014 - 2015 Robin Leffmann <djinn at stolendata dot net>
 
     https://github.com/stolendata/rpc-ace/
 
@@ -188,25 +248,27 @@ echo <<<END
 END;
 
 if( empty($query) || ctype_digit($query) )
-    echo '<meta http-equiv="refresh" content="' . REFRESHTIME . '; url=' . basename( __FILE__ ) . "\" />\n";
-echo '<title>' . COINNAME . ' block explorer &middot; RPC Ace v' . ACEVERSION . "</title>\n";
+    echo '<meta http-equiv="refresh" content="' . REFRESH_TIME . '; url=' . basename( __FILE__ ) . "\" />\n";
+echo '<title>' . COIN_NAME . ' block explorer &middot; RPC Ace v' . ACE_VERSION . "</title>\n";
 
 echo <<<END
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Varela" rel="stylesheet" type="text/css">
 <style type="text/css">
-html { background-color: #e0e0e0;
-       color: #303030;
-       font-family: Montserrat, sans-serif;
+html { height: 100%;
+       background: linear-gradient( to bottom, #80a4b4, #13437a );
+       background-attachment: fixed;
+       color: #f6f6f6;
+       font-family: Varela, sans-serif;
        font-size: 17px;
        white-space: pre; }
-a { color: #303030; }
+a { color: #f6f6f6; }
 div.mid { width: 900px;
           margin: 2% auto; }
 td { width: 16%; }
 td.urgh { width: 100%; }
 td.key { text-align: right; }
 td.value { padding-left: 16px; width: 100%; }
-tr.illu:hover { background-color: #c8c8c8; }
+tr.illu:hover { background-color: #303030; }
 </style>
 </head>
 <body>
@@ -214,26 +276,26 @@ tr.illu:hover { background-color: #c8c8c8; }
 END;
 
 // header
-echo '<table><tr><td class="urgh"><b><a href="' . COINHOME . '" target="_blank">' . COINNAME . '</a></b> block explorer</td><td>Blocks:</td><td><a href="?' . $ace['num_blocks'] . '">' . $ace['num_blocks'] . '</a>';
+echo '<table><tr><td class="urgh"><b><a href="' . COIN_HOME . '" target="_blank">' . COIN_NAME . '</a></b> block explorer</td><td>Blocks:</td><td><a href="?' . $ace['num_blocks'] . '">' . $ace['num_blocks'] . '</a>';
 $diffNom = 'Difficulty';
 $diff = sprintf( '%.3f', $ace['current_difficulty_pow'] );
-if( COINPOS )
+if( COIN_POS )
 {
     $diffNom .= ' &middot; PoS';
     $diff .= ' &middot;' . sprintf( '%.1f', $ace['current_difficulty_pos'] );
 }
 echo "<tr><td></td><td>$diffNom:</td><td>$diff</td></tr>";
-echo '<tr><td>Powered by <a href="https://github.com/stolendata/rpc-ace/" target="_blank">RPC Ace</a> v' . ACEVERSION . ' (RPC AnyCoin Explorer)</td><td>Network hashrate: </td><td>' . $ace['hashrate_mhps'] . ' MH/s</td></tr><tr><td> </td><td></td><td></td></tr></table>';
+echo '<tr><td>Powered by <a href="https://github.com/stolendata/rpc-ace/" target="_blank">RPC Ace</a> v' . ACE_VERSION . ' (RPC AnyCoin Explorer)</td><td>Network hashrate: </td><td>' . $ace['hashrate_mhps'] . ' MH/s</td></tr><tr><td> </td><td></td><td></td></tr></table>';
 
 // list of blocks
 if( isset($ace['blocks']) )
 {
     echo "<table><tr><td><b>Block</b></td><td><b>Hash</b></td><td><b>$diffNom</b></td><td><b>Time (UTC)</b></td><td><b>Tx# &middot; Value out</b></td></tr><tr><td colspan=\"5\"></td></tr>";
     foreach( $ace['blocks'] as $block )
-        echo "<tr class=\"illu\"><td>{$block['height']}</td><td><a href=\"?{$block['hash']}\">" . substr( $block['hash'], 0, 16 ) . ' ...</a></td><td>' . sprintf( '%.2f', $block['difficulty'] ) . "</td><td><a title=\"{$block['time']}\">{$block['date']}</a></td><td>{$block['tx_count']} &middot; " . sprintf( '%.2f', $block['total_out'] ) . '</td></tr>';
+        echo "<tr class=\"illu\"><td>{$block['height']}</td><td><a href=\"?{$block['hash']}\">" . substr( $block['hash'], 0, 16 ) . '&hellip;</a></td><td>' . sprintf( '%.2f', $block['difficulty'] ) . "</td><td>{$block['date']}</td><td>{$block['tx_count']} &middot; " . sprintf( '%.2f', $block['total_out'] ) . '</td></tr>';
 
-    $newer = $query < $ace['num_blocks'] ? '<a href="?' . ( $ace['num_blocks'] - $query >= BLOCKSPERLIST ? $query + BLOCKSPERLIST : $ace['num_blocks'] ) . '">&lt; Newer</a>' : '&lt; Newer';
-    $older = $query - count( $ace['blocks'] ) >= 0 ? '<a href="?' . ( $query - BLOCKSPERLIST ) . '">Older &gt;</a>' : 'Older &gt;';
+    $newer = $query < $ace['num_blocks'] ? '<a href="?' . ( $ace['num_blocks'] - $query >= BLOCKS_PER_LIST ? $query + BLOCKS_PER_LIST : $ace['num_blocks'] ) . '">&lt; Newer</a>' : '&lt; Newer';
+    $older = $query - count( $ace['blocks'] ) >= 0 ? '<a href="?' . ( $query - BLOCKS_PER_LIST ) . '">Older &gt;</a>' : 'Older &gt;';
 
     echo "<tr><td colspan=\"5\" class=\"urgh\"> </td></tr><tr><td colspan=\"5\">$newer          $older</td></tr></table>";
 }
